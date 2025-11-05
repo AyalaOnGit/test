90b9c225cb4a613b76f652e30ba5b3d76e0f1d2b
AyalaOnGit <arh67637@gmail.com>
2025-11-05 23:24:55 +0200

    small change-in alert in func branch1

--- branch1.js
+++ branch1.js
@@ -1,7 +1,7 @@
 alert("this is change from branch 1")
 
 const branch1 = () => {
-    alert("i am doing a small change that will not cause a conflict")
+    alert("i am doing a small change that will not cause a conflict!!!!!!!!!!!!!")
 }
 
 const branch11 = () => {
